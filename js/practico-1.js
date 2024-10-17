0f33b8d39070e657212b90c52d9b43a088167e46
Matias <matiassanchez13422@gmail.com>
2024-10-17 12:21:33 -0300

    Se realizo el porcentaje total de calificaciones

--- js/practico-1.js
+++ js/practico-1.js
@@ -1,1 +1,9 @@
+const respuestas = [8, 5, 10, 7, 6, 8, 9, 10, 7, 4, 6, 3, 7, 8, 6, 5, 4, 2, 9, 10]
+let suma = 0;
 
+for (let i=0; i < respuestas.length; i++){
+    suma += respuestas[i];
+}
+ 
+porcentaje = suma / 20
+console.log(porcentaje)
